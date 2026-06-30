f3efce17c5a355f142d789adaa7b00d4dd023d9d
mark.mcardle <mark.mcardle@workhuman.com>
2026-06-30 09:48:55 +0100

    Initial commit: first JUnit 5 test passing

---
+++ src/test/java/com/mark/MyFirstTest.java
@@ -0,0 +1,12 @@
+package com.mark;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyFirstTest {
+
+    @Test
+    void onePlusOneShouldEqualTwo() {
+        assertEquals(2, 1 + 1);
+    }
+}
